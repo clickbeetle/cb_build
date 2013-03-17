09d35235d9ede7838a5bf59e1b8fac20d144a3a3
root <shrinidhi666@gmail.com>
2013-03-17 23:07:01 +0530

    adding keychain

--- build_all.py
+++ build_all.py
@@ -12,5 +12,5 @@ if(os.system("mkdir -p /BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid
   print("making build dir failed : /BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid))
 os.system("/home/shrinidhi/bin/gitHub/cb_build/stage1.py --root=/BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid) +" --configroot=/home/shrinidhi/bin/gitHub/cb_build/configRoot/ --profile=default/linux/amd64/10.0 --arch=amd64 --subarch=\"\" --keywords=~amd64")
 os.system("/home/shrinidhi/bin/gitHub/cb_build/stage2.py --root=/BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid))
-os.system("/home/shrinidhi/bin/gitHub/cb_build/stage3.py --root=/BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid) +" --pkgs app-portage/gentoolkit,sys-apps/gptfdisk,app-portage/eix,app-admin/sudo,sys-process/time,net-misc/bridge-utils,sys-apps/usermode-utilities")
+os.system("/home/shrinidhi/bin/gitHub/cb_build/stage3.py --root=/BACKUP/clickbeetleCook.DO_NO_DELETE/cb_build/"+ str(uuid) +" --pkgs app-portage/gentoolkit,sys-apps/gptfdisk,app-portage/eix,app-admin/sudo,sys-process/time,net-misc/bridge-utils,sys-apps/usermode-utilities,net-misc/keychain")
 print uuid
